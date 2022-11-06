e7a7088f6bf276a60196da47e7c58189891d6199
edenmor1989 <79272698+edenmor1989@users.noreply.github.com>
2022-11-06 21:55:52 +0200

    add test.py

---
+++ test.py
@@ -0,0 +1,62 @@
+import requests
+from datetime import date
+from dateutil.relativedelta import relativedelta
+import json2html
+from json2html import *
+import json
+import os
+datetoday=date.today()
+d1=datetoday.strftime("%Y-%m-%d")
+print(d1)
+date3months=datetoday+relativedelta(months=+3)
+print (date3months)
+url = f'https://www.hebcal.com/hebcal?v=1&cfg=json&maj=on&min=on&mod=on&nx=off&year=now&month=x&start={d1}&end=2022-12-30&ss=off&mf=off&c=on&'
+resp = requests.get(url=url)
+data = resp.json()
+data = json.dumps(data, indent = 4)
+print(type(data))
+result={'items':[]}
+x=[]
+resultlist=[]
+import os
+if os.path.exists("output.html"):
+  os.remove("output.html")
+else:
+  print("The file does not exist")
+data=eval(data)
+for item in data['items']:
+    result['items'].append({'title':item['title'],'date':item['date']})
+fl=open('output.txt', 'w')
+json.dump(result,fl,indent=4)
+fl.close()
+print(result)
+#json_load = json.loads(data)
+import os
+if os.path.exists("output.json"):
+  os.remove("output.json")
+else:
+  print("The file does not exist")
+
+#json2html.convert(json=result)
+#print(result)
+#exit()
+import http.server
+import socketserver
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import ssl
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = 'output.txt'
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+# Create an object of the above class
+handler_object = MyHttpRequestHandler
+
+PORT = 8082
+my_server = socketserver.TCPServer(("", PORT), handler_object)
+
+# Star the server
+my_server.serve_forever()
+
+print(result)
